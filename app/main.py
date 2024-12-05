b3e7debfe0439846e62e54a8db13af88ee209fdb
Koziakov Hlib <hlibs.work@gmail.com>
2024-12-05 21:39:32 +0200

    Implemented classes Animal(parent) and Carnivore and Herbivore

--- app/main.py
+++ app/main.py
@@ -1,1 +1,30 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}," \
+               f" Hidden: {self.hidden}}}"
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    @staticmethod
+    def bite(herb: Herbivore | Carnivore) -> None:
+        if isinstance(herb, Herbivore)\
+                and herb.hidden is False:
+            herb.health -= 50
+        if herb.health <= 0:
+            Animal.alive.remove(herb)
